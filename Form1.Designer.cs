9fb789487c3c96139d829f3eb8f9e4b202f22035
KormyshRoman <ipz204_kri@student.ztu.edu.ua>
2021-03-12 21:32:25 +0200

    Додано прототип програми для створення фракталів(Записано функцыю для створення Множини Мандельброта, клас для комплексних чисел та добавлено кольорову візуалізацію)

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image
+            // 
+            this.image.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.image.Location = new System.Drawing.Point(0, 0);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(1264, 681);
+            this.image.TabIndex = 0;
+            this.image.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1146, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+/// <summary>
+/// 1)  Попробувати реалізувати приближення по алгоратму 
+/// if(newWidth < newHeight)
+/// {
+///     for(int i = newWidth; newWidth < oldWidth; newWidth++)
+///     {
+///         zoom -= zoomSpeed / zoom;
+///     }
+/// }
+/// else
+/// {
+/// 
+///      for(int i = newHeight; newHeight < oldHeight; newHeight++)
+///      {
+///         zoom -= zoomSpeed / zoom;
+///      }
+/// }
+/// 
+/// 2) Додати розфарбування фракталу за допомогою градієнту
+/// 3) Спробувати ререндирити картинку для виконання зумузуму
+/// /// </summary>
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public double wx = -150, wy = 0;
+        public double speed = 2f, zoom = 2.4f, zoomSpeed = 0.1d;
+        public int res = 1;
+        public double screenSize = 16 / 9.0;
+        int UserIt = 100;
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                wy -= speed * (5 - Math.Abs(zoom));
+                DrawMBrot();
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                wy += speed * (5 - Math.Abs(zoom));
+                DrawMBrot();
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                wx -= speed * (5 - Math.Abs(zoom));
+                DrawMBrot();
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                wx += speed * (5 - Math.Abs(zoom));
+                DrawMBrot();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void DrawMBrot()
+        {
+            if (res <= 0)
+            {
+                res = 1;
+            }
+
+            Bitmap picture = new Bitmap(Width / res, Height / res);
+            for (int x = 0; x < Width/res; x++)
+            {
+                for (int y = 0; y < Height/res; y++)
+                {
+                    double a = (double)((x + (wx / res / Math.Abs(zoom))) - ((Width / 2d) / res)) / (double)(Width / Math.Abs(zoom) / res / screenSize);
+                    double b = (double)((y + (wy / res / Math.Abs(zoom))) - ((Height / 2d) / res)) / (double)(Height / Math.Abs(zoom) / res);
+
+                    Complex c = new Complex(a , b);
+                    Complex z = new Complex(0, 0);
+
+
+                    int it = 0;
+
+                    do
+                    {
+                        it++;
+                        z.Sqr();
+                        z.Add(c);
+                        if (z.Magn() > 4.0d)
+                        {
+                            break;
+                        }
+                    } while (it < UserIt);
+                    if (it < (int)(UserIt * 0.05))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(147, 230, 212));
+                    }
+                    if (it < (int)(UserIt * 0.07))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(255, 191, 151));
+                    }
+                    if (it < (int)(UserIt * 0.09))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(255, 187, 138));
+                    }
+                    else if (it < (int)(UserIt * 0.1))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(209, 50, 103));
+                    }
+                    else if (it < (int)(UserIt * 0.11))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(117, 239, 0));
+                    }
+                    else if (it < (int)(UserIt * 0.14))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(166, 222, 230));
+                    }
+                    else if (it < (int)(UserIt * 0.16))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(255, 182, 36));
+                    }
+                    else if (it < (int)(UserIt * 0.17))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(117, 0, 132));
+                    }
+                    else if (it < (int)(UserIt * 0.18))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(255, 141, 36));
+                    }
+                    else if (it < (int)(UserIt * 0.19))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(117, 0, 132));
+                    }
+                    else if (it < (int)(UserIt * 0.2))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(250, 179, 122));
+                    }
+                    else if (it < (int)(UserIt * 0.23))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(1, 147, 154));
+                    }
+                    else if (it < (int)(UserIt * 0.26))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(1, 149, 154));
+                    }
+                    else if (it < (int)(UserIt * 0.3))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(125, 125, 125));
+                    }
+                    else if (it < (int)(UserIt * 0.4))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(210, 53, 210));
+                    }
+                    else if (it < (int)(UserIt * 0.5))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(23, 97, 125));
+                    }
+                    else if (it < (int)(UserIt * 0.6))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(255, 204, 115));
+                    }
+                    else if (it < (int)(UserIt * 0.7))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(225, 225, 225));
+                    }
+                    else if (it < (int)(UserIt * 0.8))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(86, 225, 225));
+                    }
+                    else if (it < (int)(UserIt * 0.90))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(237, 65, 56));
+                    }
+                    else if (it < (int)(UserIt * 0.93))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(224, 53, 80));
+                    }
+                    else if (it < (int)(UserIt * 0.96))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(0, 34, 225));
+                    }
+                    else if (it < (int)(UserIt * 0.99))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(48, 50, 133));
+                    }
+                    else if (it < (int)(UserIt))
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(117, 239, 0));
+                    }
+                    else
+                    {
+                        picture.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+                }
+                image.Image = picture;
+                image.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            zoom -= zoomSpeed / zoom;
+            wx -= speed * (5 - Math.Abs(zoom));
+            DrawMBrot();
+        }
+    }
+
+
+
+    public class Complex {
+        public double a;
+        public double b;
+
+        public Complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Sqr()
+        {
+            double tmp = (a * a) - (b * b );
+            b = 2.0d * a * b;
+            a = tmp;
+        }
+
+        public double Magn()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+
+        public void Add(Complex c)
+        {
+            a += c.a;
+            b += c.b;
+        }
+        
+    }
+
+}
